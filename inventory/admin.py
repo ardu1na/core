96f5dc4764d052204d4f4c97cc27237a46d6bec8
ardu1na <arduinadelbosque@gmail.com>
2023-03-10 00:01:55 -0300

    new requirements for django-jazzmin admin panel ui2

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from inventory.models import Department, Category, Item
+from inventory.models import Department, Category, Item, Inventory
 
 # define info show in admin panel
 admin.site.site_header = 'College Inventory System'
@@ -7,9 +7,28 @@ admin.site.index_title = 'Home'
 admin.site.site_title = 'College Inventory System'
 
 
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
-admin.site.register(Department)
-                   
-admin.site.register(Category)
-                     
-admin.site.register(Item)
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+    search_fields = ('name', 'department__name')
+    list_filter = ('department',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at', 'category')
+    search_fields = ('name', 'category__name', 'inventory__name')
+    list_filter = ('inventory__department', 'category')
+    filter_horizontal = ('inventory',)
